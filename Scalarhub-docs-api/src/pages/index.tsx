8f4ae5f1128229de694e24e93b70baa53e0043ba
Manasa Kurella <your.email@example.com>
2024-10-26 18:47:54 +0530

    Update index.tsx

--- Scalarhub-docs-api/src/pages/index.tsx
+++ Scalarhub-docs-api/src/pages/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
-// import  Link  from '@docusaurus/Link';
+// import React from 'react';
+// import { Redirect } from '@docusaurus/router';
+// // import  Link  from '@docusaurus/Link';
            
 
-// Default export for Home component
-export default function Home(): JSX.Element {
-  return <Redirect to="/guides/Overview" />;
-}
+// // Default export for Home component
+// export default function Home(): JSX.Element {
+//   return <Redirect to="/guides/Overview" />;
+// }
+
+
+import React, { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
+
+const Home: React.FC = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    // Redirect to the desired path
+    history.push('/guides/Overview');
+  }, [history]);
+
+  return null;
+};
+
+export default Home;
